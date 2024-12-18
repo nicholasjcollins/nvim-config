e948727687bc1bc84c9d8ccf535d32de2872b48c
Nick <nick@smplfi.com>
2024-12-18 10:49:33 -0500

    swapping neotree for oil, general cleanup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,8 +2,9 @@ return {
   {
     'echasnovski/mini.nvim',
     config = function()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = true }
+      require('mini.statusline').setup { use_icons = true }
+      require('mini.jump').setup {}
+      require('mini.jump2d').setup {}
     end,
   },
 }
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  },
+
+  vim.keymap.set('n', '-', '<cmd>Oil<CR>'),
+}
